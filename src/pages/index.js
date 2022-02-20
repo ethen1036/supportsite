6199ebc19d92fd1a927cd85e435d57ff3f88703a
Shubham Kumar <sraj94710@gmail.com>
2022-02-20 19:28:41 +0530

    sample website clone

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ThemeProvider } from 'theme-ui';
+import theme from 'theme';
+import SEO from 'components/seo';
+import Layout from 'components/layout';
+import Banner from 'sections/banner';
+import Support from 'sections/support';
+import PremiumFeature from 'sections/premium-feature';
+import AppFeature from 'sections/app-feature';
+import Dashboard from 'sections/dashboard';
+import Pricing from 'sections/pricing';
+import Testimonials from 'sections/testimonials';
+import Subscribe from 'sections/subscribe';
+
+export default function IndexPage() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <SEO
+          title="OpenSupport | Real human support | Web, Mobile, Cloud"
+          description="Real human support for all your technical issues related | No lengthy knowledgebase"
+        />
+        <Banner />
+        <Support />
+        <PremiumFeature />
+        <AppFeature />
+        <Dashboard />
+        <Pricing />
+        {/* <Testimonials /> */}
+        <Subscribe />
+      </Layout>
+    </ThemeProvider>
+  );
+}
